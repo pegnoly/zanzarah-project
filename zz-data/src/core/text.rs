ca363afc556ea44462e8475af5d4da024d940021
Victor Romanov <gerter666@hotmail.com>
2024-09-06 00:57:09 +0300

    fix of text struct had private fields

--- zz-data/src/core/text.rs
+++ zz-data/src/core/text.rs
@@ -3,9 +3,9 @@ use serde::{Deserialize, Serialize};
 /// Represents text parsed from game files.
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Text {
-    id: String,
-    content_length: i32,
-    content: String,
-    text_type: i32,
-    mark: String
+    pub id: String,
+    pub content_length: i32,
+    pub content: String,
+    pub text_type: i32,
+    pub mark: String
 }
