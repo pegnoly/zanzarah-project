b800617b3f50dd5aa2205085f76f59e772ffcca5
pegnoly <owafe001@gmail.com>
2025-05-02 18:31:14 +0300

    smth i didn't commit before(new pc transfer)

--- zz-parser/src-tauri/src/app/commands.rs
+++ zz-parser/src-tauri/src/app/commands.rs
@@ -13,16 +13,24 @@ pub async fn start_parsing(
 ) -> Result<(), ZZParserError> {
     let current_book_config = app_config.books_data.get(&app_config.current_book).unwrap();
     let mut parser = ParseProcessor::new(&current_book_config.name_plugins, &current_book_config.desc_plugins);
-    parser.parse_texts(PathBuf::from(current_book_config.directory.clone()))?;
+    parser
+        .parse_texts(PathBuf::from(current_book_config.directory.clone()))
+        .inspect_err(|error| {
+            log::error!("Failed to parse texts: {:#?}", error);
+        })?;
     let wizforms = parser
-        .parse_wizforms(PathBuf::from(current_book_config.directory.clone()), app_config.current_book)?;
+        .parse_wizforms(PathBuf::from(current_book_config.directory.clone()), app_config.current_book)
+        .inspect_err(|error| {
+            log::error!("Failed to parse wizforms: {:#?}", error);
+        })?;
     match zanzarah_service.upload_wizforms(wizforms).await {
         Ok(success) => {
-            println!("Wizforms uploaded successfully: {}", success.message)
+            log::info!("Wizforms upload result: {}", success.message);
+            Ok(())
         },
         Err(error) => {
-            println!("Wizforms upload failed: {:#?}", error)
+            log::error!("Failed to upload wizforms: {:#?}", &error);
+            Err(error)
         }
-    };
-    Ok(())
+    }
 }
