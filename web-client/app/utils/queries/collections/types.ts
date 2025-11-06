3e66ad963c5e2260caf6f7d82f59cb6abd620cc1
pegn0ly <owafe001@gmail.com>
2025-11-06 09:21:06 +0300

    old web client remove

--- web-client/app/utils/queries/collections/types.ts
+++
@@ -1,9 +0,0 @@
-export type CollectionModel = {
-    id: string,
-    bookId: string,
-    name: string,
-    description: string | null,
-    createdOnVersion: string,
-    active: boolean,
-    entriesCount: number
-}
