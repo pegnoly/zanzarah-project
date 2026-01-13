a092d474f7077b61f8fc49256b8375f88322717c
pegn0ly <owafe001@gmail.com>
2026-01-13 14:32:03 +0300

    update endpoint

--- web-client/src/queries/common.ts
+++ web-client/src/queries/common.ts
@@ -1,2 +1,2 @@
-// export const API_ENDPOINT = "https://zanzarah-project-api-lyaq.shuttle.app/"
-export const API_ENDPOINT = "http://127.0.0.1:3000";
+export const API_ENDPOINT = "https://zanzarah-project-production.up.railway.app"
+// export const API_ENDPOINT = "http://127.0.0.1:3000";
